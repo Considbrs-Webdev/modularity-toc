52f930dac1540cbb21a9f70fb653b45073b0ccb9
Michael Claesson <michael.claesson@consid.se>
2026-01-27 16:30:56 +0100

    Fixed build script

--- build.php
+++ build.php
@@ -5,24 +5,10 @@ if (php_sapi_name() !== 'cli') {
     exit(0);
 }
 
-/* Parameters: 
- --no-composer      Does not install vendors via composer
- --cleanup          Remove removeables
- --install-npm      Installs npm package as per package.json name field
- --release          Does not run composer install and does not remove .git
-*/
-
-// Any command needed to run and build plugin assets when newly cheched out of repo.
 $buildCommands = [];
 
-//Add composer build, if flag --no-composer is undefined.
-//Dump autloader. 
-//Only if composer.json exists.
+//Run composer if composer.json is found
 if (file_exists('composer.json')) {
-    if (is_array($argv) && !in_array('--no-composer', $argv)) {
-        $buildCommands[] = 'composer install --prefer-dist --no-progress --no-dev';
-    }
-
     $buildCommands[] = 'composer dump-autoload';
 }
 
@@ -32,10 +18,8 @@ if (file_exists('package.json') && file_exists('package-lock.json')) {
         $buildCommands[] = 'npm ci --no-progress --no-audit';
         $buildCommands[] = 'npm run build';
     } else {
-        $npmPackage = json_decode(file_get_contents('package.json'));
-        $buildCommands[] = "npm install $npmPackage->name";
-        $buildCommands[] = "rm -rf ./dist";
-        $buildCommands[] = "mv node_modules/$npmPackage->name/dist ./";
+        $buildCommands[] = "npm install --no-progress --no-audit";
+        $buildCommands[] = "npm run build";
     }
 } elseif (file_exists('package.json') && !file_exists('package-lock.json')) {
     if (is_array($argv) && !in_array('--install-npm', $argv)) {
@@ -43,28 +27,24 @@ if (file_exists('package.json') && file_exists('package-lock.json')) {
         $buildCommands[] = 'npm run build';
     } else {
         $npmPackage = json_decode(file_get_contents('package.json'));
-        $buildCommands[] = "npm install $npmPackage->name";
-        $buildCommands[] = "rm -rf ./dist";
-        $buildCommands[] = "mv node_modules/$npmPackage->name/dist ./";
+        $buildCommands[] = "npm install --no-progress --no-audit";
+        $buildCommands[] = "npm run build";
     }
 }
 
 // Files and directories not suitable for prod to be removed.
 $removables = [
     '.gitignore',
-    '.github',
-    '.gitattributes',
     'build.php',
-    '.npmrc',
-    //'composer.json',
-    'composer.lock',
-    'env-example',
-    'package-lock.json',
+    'vite.config.js',
+    'vite.config.mjs',
     'package.json',
+    'package-lock.json',
+    'phpunit.xml.dist',
     'README.md',
     './node_modules/',
-    './source/sass/',
     './source/js/',
+    './source/sass/',
     'LICENSE',
 ];
 
